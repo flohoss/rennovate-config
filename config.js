35100dd01744fe72030fbd1cf6de4c63fb6c0b98
Florian Hoss <mail@florianhoss.de>
2025-09-10 22:46:11 +0200

    Revert "Limit to docker-compose repo"
    This reverts commit ba690e194c9006b7c358cb1dce9e14f4f96e09c2.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     gitAuthor: 'Renovate Bot <renovate@unjx.de>',
     username: 'renovate[bot]',
     autodiscover: true,
-    autodiscoverFilter: 'flohoss/docker-compose',
+    autodiscoverFilter: 'flohoss/*',
     persistRepoData: true,
     dependencyDashboard: true,
 
