32a169434f19d2a7220d1cd433ae3fc4338f42f7
Florian Hoss <mail@florianhoss.de>
2026-02-06 19:02:49 +0100

    Use double quotes for postUpgradeTasks command
    Switch the curl command in postUpgradeTasks from a single-quoted string to a double-quoted string and escape the internal double quotes. This preserves the embedded header/value quoting in the curl invocation and avoids potential parsing/escape issues.

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
     allowedCommands: ['^curl -s -X POST .*'],
     postUpgradeTasks: {
         commands: [
-            'curl -s -X POST "$RENOVATE_NTFY_URL" -H "Authorization: Bearer $RENOVATE_NTFY_TOKEN" -H "X-Tags: twisted_rightwards_arrows" -H "X-Title: Renovate PR - {{{depName}}}" -d "A Renovate PR has been created/updated for {{{depName}}} ({{{currentVersion}}} → {{{newVersion}}})"'
+            "curl -s -X POST \"$RENOVATE_NTFY_URL\" -H \"Authorization: Bearer $RENOVATE_NTFY_TOKEN\" -H \"X-Tags: twisted_rightwards_arrows\" -H \"X-Title: Renovate PR - {{{depName}}}\" -d \"A Renovate PR has been created/updated for {{{depName}}} ({{{currentVersion}}} → {{{newVersion}}})\""
         ],
         executionMode: 'branch'
     }
