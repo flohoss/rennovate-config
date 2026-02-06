27f37229733782b1cbc28e90493e059a0bf6aa53
Florian Hoss <mail@florianhoss.de>
2026-02-06 18:57:00 +0100

    Add hostType 'docker' to hostRules
    Set hostType to 'docker' for the hostRules matching docker.io and ghcr.io so these registry entries are explicitly treated as Docker registries. Credentials continue to be sourced from RENOVATE_DOCKER_HUB_PASSWORD and RENOVATE_GHCR_TOKEN environment variables.

--- config.js
+++ config.js
@@ -8,11 +8,13 @@ module.exports = {
     requireConfig: 'optional',
     hostRules: [
         {
+            hostType: 'docker',
             matchHost: 'docker.io',
             username: 'unjxde',
             password: process.env.RENOVATE_DOCKER_HUB_PASSWORD,
         },
         {
+            hostType: 'docker',
             matchHost: 'ghcr.io',
             username: 'flohoss',
             password: process.env.RENOVATE_GHCR_TOKEN,
