f824bd7a6ab21c193e8845539966ff2219780997
Florian Hoss <mail@florianhoss.de>
2026-02-10 08:42:14 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    platform: 'github',
+    token: process.env.RENOVATE_TOKEN,
+    gitAuthor: `${process.env.RENOVATE_AUTHOR} <${process.env.RENOVATE_PRIVATE_EMAIL}>`,
+    username: process.env.RENOVATE_USERNAME,
+    repositories: [`${process.env.RENOVATE_USERNAME}/docker-compose`, `${process.env.RENOVATE_USERNAME}/stacks`],
+    onboarding: false,
+    requireConfig: 'optional',
+    hostRules: [
+        {
+            matchHost: 'docker.io',
+            username: process.env.RENOVATE_DOCKER_HUB_USERNAME,
+            password: process.env.RENOVATE_DOCKER_HUB_PASSWORD,
+        },
+        {
+            matchHost: 'ghcr.io',
+            username: process.env.RENOVATE_GHCR_USERNAME,
+            password: process.env.RENOVATE_GHCR_TOKEN,
+        }
+    ],
+    allowedCommands: ['^curl -s -X POST .*'],
+    postUpgradeTasks: {
+        commands: [
+            `curl -s -X POST "${process.env.RENOVATE_NTFY_URL}" -H "Authorization: Bearer ${process.env.RENOVATE_NTFY_TOKEN}" -H "X-Tags: twisted_rightwards_arrows" -H "X-Title: Renovate - {{{depName}}}" -d "{{{prTitle}}}"`
+        ],
+        executionMode: 'branch'
+    }
+};
